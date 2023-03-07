db668392694f73da46edbdb8f4c6828b0eaa29f3
Benedetto Pellerito <svilupporsasantangelo@gmail.com>
2023-03-07 02:00:01 +0100

    commit file list file optimiz download

--- src/main/java/online/vrscuola/controllers/DownloadController.java
+++ src/main/java/online/vrscuola/controllers/DownloadController.java
@@ -1,41 +1,56 @@
 package online.vrscuola.controllers;
 
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DownloadController {
 
+    private static final Path RESOURCE_DIR = Paths.get("/var/lib/tomcat9/resources");
+    private static final CacheControl CACHE_CONTROL = CacheControl.maxAge(10, TimeUnit.MINUTES);
+
     @GetMapping("/resources/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) throws IOException, IOException {
+    @Cacheable("fileContentCache")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable String fileName) throws IOException {
+        Path filePath = RESOURCE_DIR.resolve(fileName);
 
-        // Ottieni il path assoluto del file da scaricare
-        String filePath = "/var/lib/tomcat9/resources/" + fileName;
-
-        // Crea un oggetto Resource che rappresenta il file
-        Resource fileResource = new FileSystemResource(filePath);
-
-        // Verifica se il file esiste
-        if (!fileResource.exists()) {
-            throw new RuntimeException("File non trovato");
+        if (!Files.exists(filePath)) {
+            return ResponseEntity.notFound().build();
         }
 
-        // Imposta il content type della risposta HTTP in base all'estensione del file
-        String contentType = request.getServletContext().getMimeType(fileResource.getFile().getPath());
+        MediaType mediaType = MediaTypeFactory.getMediaType(filePath.toString())
+                .orElse(MediaType.APPLICATION_OCTET_STREAM);
 
-        // Crea la risposta HTTP con il contenuto del file
+        byte[] fileContent = Files.readAllBytes(filePath);
+        long contentLength = Files.size(filePath);
+
         return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileResource.getFilename() + "\"")
-                .body(fileResource);
+                .cacheControl(CACHE_CONTROL)
+                .contentType(mediaType)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .header(HttpHeaders.CONTENT_LENGTH, Long.toString(contentLength))
+                .body(fileContent);
+    }
+
+    @RestControllerAdvice
+    public static class ExceptionHandlerAdvice {
+
+        @ExceptionHandler(IOException.class)
+        @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+        public String handleIOException(IOException e) {
+            return "Errore durante il download del file: " + e.getMessage();
+        }
+
+        @ExceptionHandler(Exception.class)
+        @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+        public String handleException(Exception e) {
+            return "Errore generico durante il download del file: " + e.getMessage();
+        }
     }
 }
---
+++ src/main/java/online/vrscuola/controllers/ResourceController.java
@@ -0,0 +1,71 @@
+package online.vrscuola.controllers;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+
+    private static final Path RESOURCE_DIR = Paths.get("/var/lib/tomcat9/resources");
+
+    @GetMapping(value = "/resources/files", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ResourceInfo>> getResourcesFiles() {
+        List<ResourceInfo> resources = new ArrayList<>();
+        try {
+            File directory = RESOURCE_DIR.toFile();
+            if (directory.isDirectory()) {
+                File[] files = directory.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        if (file.isFile()) {
+                            resources.add(new ResourceInfo(file.getName(), file.length(), getMimeType(file)));
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // Gestione dell'eccezione
+        }
+        return ResponseEntity.ok(resources);
+    }
+
+    private String getMimeType(File file) {
+        // Recupera il tipo MIME del file a partire dal nome del file o dall'estensione
+        // Utilizza la classe org.springframework.http.MediaTypeFactory per ottenere il tipo MIME
+        return MediaTypeFactory.getMediaType(file.getName())
+                .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                .toString();
+    }
+
+    public static class ResourceInfo {
+        private String name;
+        private long size;
+        private String type;
+
+        public ResourceInfo(String name, long size, String type) {
+            this.name = name;
+            this.size = size;
+            this.type = type;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public long getSize() {
+            return size;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }
+}
