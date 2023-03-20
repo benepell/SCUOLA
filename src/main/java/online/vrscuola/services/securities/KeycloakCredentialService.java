aeb200fa3d6d4bab90d867e56033f02b277e15cd
Benedetto Pellerito <svilupporsasantangelo@gmail.com>
2023-03-21 00:42:18 +0100

    aggiona algoritmo password bridge

--- src/main/java/online/vrscuola/services/securities/KeycloakCredentialService.java
+++ src/main/java/online/vrscuola/services/securities/KeycloakCredentialService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
@@ -26,6 +27,7 @@ public class KeycloakCredentialService {
         return result;
     }
 
+
     public String generateKeycloakBridgeCredentials(String resource, String username)
     {
         int multiply = 1;
@@ -33,9 +35,9 @@ public class KeycloakCredentialService {
             multiply *= (int) c;
         }
         String result = String.valueOf(multiply);
-        result = (int) username.charAt(0) + result;
+        result =  username.charAt(0) + result;
 
-        return Base64.getEncoder().encodeToString(result.getBytes());
+        return Base64.getEncoder().encodeToString(result.getBytes(StandardCharsets.UTF_8));
     }
 
 }
