6af9cf804666650ae6d9447f12e4328c401e8947
Benedetto Pellerito <svilupporsasantangelo@gmail.com>
2023-04-07 14:24:29 +0200

    stato di salute controlla ogni ora scipt fix

--- scripts/opt/check_service.sh
+++ scripts/opt/check_service.sh
@@ -12,7 +12,6 @@ if [[ ! -d /opt/script/log/ ]]; then
   mkdir -p /opt/script/log/
 fi
 
-
 # Controlla se l'applicazione è presente nella directory /var/lib/tomcat9/webapps/api/
 if [[ ! -e /var/lib/tomcat9/webapps/api/ ]]; then
   echo "$timestamp [error] L'applicazione non è presente nella directory /var/lib/tomcat9/webapps/api/. Eseguo il comando per riavviare il servizio Tomcat9 e attendo 3 minuti prima di riavviare lo script." >> /var/log/myapp.log
@@ -30,45 +29,57 @@ status_resourceDirectory=$(echo "$response" | jq -r '.resourceDirectory.status')
 status_websiteKeycloak=$(echo "$response" | jq -r '.websiteKeycloak.status')
 status_operatingSystem=$(echo "$response" | jq -r '.operatingSystem.status')
 
-# Controlla se la risposta dall'endpoint è null
-if [[ -z "$response" ]]; then
-  echo "$timestamp [error] La risposta dall'endpoint è null. Eseguo il comando per riavviare il servizio Tomcat9 e attendo 3 minuti prima di riavviare lo script." >> /var/log/myapp.log
-  echo "$timestamp [error] Stato dei servizi: website=$status_website, database=$status_database, websiteRisorse=$status_websiteRisorse, resourceDirectory=$status_resourceDirectory, websiteKeycloak=$status_websiteKeycloak, operatingSystem=$status_operatingSystem." >> "$logfile"
-  systemctl restart tomcat9
-  sleep 180
-  exec "$0"
-fi
 
-if [[ "$status_website" != "ok" ]]; then
+# Imposta il tempo massimo di attesa in secondi (10 minuti = 600 secondi)
+MAX_WAIT_TIME=600
+WAIT_TIME=0
+
+# Esegui il ciclo while finché tomcat9 non è avviato e non è stato raggiunto il tempo massimo di attesa
+while ! pgrep -f tomcat9 > /dev/null && [ ${WAIT_TIME} -lt ${MAX_WAIT_TIME} ]; do
+    # Controlla se la risposta dall'endpoint è null
+    if [ -z "$response" ]; then
+      echo "$timestamp [error] La risposta dall'endpoint è null. Eseguo il comando per riavviare il servizio Tomcat9 e attendo 3 minuti prima di riavviare lo script." >> /var/log/myapp.log
+      echo "$timestamp [error] La risposta dall'endpoint è null. Eseguo il comando per riavviare il servizio Tomcat9." >> "$logfile"
+      systemctl restart tomcat9
+      exec "$0"
+    fi
+
+    # Attendi 15 secondi
+    sleep 15
+    # Aggiorna il tempo di attesa trascorso
+    WAIT_TIME=$((WAIT_TIME + 15))
+done
+
+if [ "$status_website" != "ok" ]; then
   echo "$timestamp [error] Il servizio website è in stato di errore. Eseguo il comando per riavviare il servizio Tomcat9." >> /var/log/myapp.log
   echo "$timestamp [error] Stato dei servizi: website=$status_website, database=$status_database, websiteRisorse=$status_websiteRisorse, resourceDirectory=$status_resourceDirectory, websiteKeycloak=$status_websiteKeycloak, operatingSystem=$status_operatingSystem." >> "$logfile"
   systemctl restart tomcat9
 fi
 
-if [[ "$status_database" != "ok" ]]; then
+if [ "$status_database" != "ok" ]; then
   echo "$timestamp [error] Il servizio database è in stato di errore. Eseguo il comando per riavviare il servizio mysql." >> /var/log/myapp.log
   echo "$timestamp [error] Stato dei servizi: website=$status_website, database=$status_database, websiteRisorse=$status_websiteRisorse, resourceDirectory=$status_resourceDirectory, websiteKeycloak=$status_websiteKeycloak, operatingSystem=$status_operatingSystem." >> "$logfile"
   systemctl restart mysql
 fi
 
-if [[ "$status_websiteRisorse" == "error" ]]; then
+if [ "$status_websiteRisorse" != "ok" ]; then
   echo "$timestamp [error] Il servizio websiteRisorse è in stato di errore. Eseguo il comando per riavviare il servizio apache2." >> /var/log/myapp.log
   echo "$timestamp [error] Stato dei servizi: website=$status_website, database=$status_database, websiteRisorse=$status_websiteRisorse, resourceDirectory=$status_resourceDirectory, websiteKeycloak=$status_websiteKeycloak, operatingSystem=$status_operatingSystem." >> "$logfile"
   systemctl restart apache2
 fi
 
-if [[ "$status_resourceDirectory" != "ok" ]]; then
+if [ "$status_resourceDirectory" != "ok" ]; then
   echo "$timestamp [error] Il servizio resourceDirectory è in stato di errore. Eseguo il comando di ripristino." >> /var/log/myapp.log
   echo "$timestamp [error] Stato dei servizi: website=$status_website, database=$status_database, websiteRisorse=$status_websiteRisorse, resourceDirectory=$status_resourceDirectory, websiteKeycloak=$status_websiteKeycloak, operatingSystem=$status_operatingSystem." >> "$logfile"
   # Inserisci qui il comando per ripristinare il servizio
 fi
 
-if [[ "$status_websiteKeycloak" != "ok" ]]; then
+if [ "$status_websiteKeycloak" != "ok" ]; then
    echo "$timestamp [error] Il servizio websiteKeycloak è in stato di errore. Eseguo il comando per riavviare il servizio Tomcat9." >> /var/log/myapp.log
    echo "$timestamp [error] Stato dei servizi: website=$status_website, database=$status_database, websiteRisorse=$status_websiteRisorse, resourceDirectory=$status_resourceDirectory, websiteKeycloak=$status_websiteKeycloak, operatingSystem=$status_operatingSystem." >> "$logfile"
    killall -9 java
    systemctl restart tomcat9
-   cd /opt/keycloak-21.0.0/bin/
+   cd /opt/keycloak/bin/
 ./start.sh &
 fi
 
