8d82b0d4fc0caa2ed66d2548953800e88f4b32e6
Benedetto Pellerito <svilupporsasantangelo@gmail.com>
2023-05-01 16:19:38 +0200

    aggiunta monteore async

--- src/main/java/online/vrscuola/services/devices/VRDeviceManagerOrderService.java
+++ src/main/java/online/vrscuola/services/devices/VRDeviceManagerOrderService.java
@@ -3,6 +3,9 @@ package online.vrscuola.services.devices;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 public class VRDeviceManagerOrderService {
@@ -10,30 +13,42 @@ public class VRDeviceManagerOrderService {
     private String[] username;
     private Long[] minutes;
     private VRDeviceManageDetailService detailService;
+    private ExecutorService executorService;
 
     public void initOrder(String[] alunni, String[] username, VRDeviceManageDetailService detailService) {
         this.alunni = alunni;
         this.username = username;
         this.detailService = detailService;
         this.minutes = new Long[alunni.length];
-        addMinutes();
+        addMinutesAsync();
     }
 
-    private void addMinutes() {
-        for (int i = 0; i < this.username.length; i++) {
-            minutes[i] = detailService.getMinutes(username[i]);
+    private void addMinutesAsync() {
+        executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+        CompletableFuture<Void>[] futures = new CompletableFuture[username.length];
+
+        for (int i = 0; i < username.length; i++) {
+            final int index = i;
+            futures[i] = CompletableFuture.supplyAsync(() -> detailService.getMinutes(username[index]), executorService)
+                    .thenAccept(min -> minutes[index] = min);
         }
 
-        // Creazione di un array di indici ordinati
+        CompletableFuture.allOf(futures).join();
+
+        executorService.shutdown();
+
+        sortArrays();
+    }
+
+    private void sortArrays() {
         Integer[] indices = new Integer[minutes.length];
         for (int i = 0; i < indices.length; i++) {
             indices[i] = i;
         }
 
-        // Ordinamento degli indici in base ai valori dei minuti
         Arrays.sort(indices, (a, b) -> Long.compare(minutes[a], minutes[b]));
 
-        // Utilizzo degli indici ordinati per riordinare gli array
         String[] sortedAlunni = new String[alunni.length];
         String[] sortedUsername = new String[username.length];
         Long[] sortedMinutes = new Long[minutes.length];
@@ -45,7 +60,6 @@ public class VRDeviceManagerOrderService {
             sortedMinutes[i] = minutes[index];
         }
 
-        // Aggiornamento degli array originali con gli array ordinati
         alunni = sortedAlunni;
         username = sortedUsername;
         minutes = sortedMinutes;
