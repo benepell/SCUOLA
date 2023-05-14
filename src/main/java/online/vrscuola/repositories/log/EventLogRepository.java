c932b22f0c8ef830eb443f9f5d2e53719f273da6
Benedetto Pellerito <benedettopellerito@gmail.com>
2023-05-14 18:26:20 +0200

    event log repository

--- src/main/java/online/vrscuola/repositories/log/EventLogRepository.java
+++ src/main/java/online/vrscuola/repositories/log/EventLogRepository.java
@@ -4,8 +4,21 @@ import online.vrscuola.entities.log.EventLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import online.vrscuola.entities.log.EventLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
+
 @SuppressWarnings("unused")
 @Repository
 public interface EventLogRepository extends JpaRepository<EventLog, Long> {
+    List<EventLog> findByUsername(String username);
+    List<EventLog> findByEvent(String event);
+    List<EventLog> findByEventDate(Instant eventDate);
+    List<EventLog> findByUsernameAndEvent(String username, String event);
+    List<EventLog> findByUsernameAndEventDate(String username, Instant eventDate);
+    List<EventLog> findByEventAndEventDate(String event, Instant eventDate);
+    List<EventLog> findByUsernameAndEventAndEventDate(String username, String event, Instant eventDate);
 }
-
