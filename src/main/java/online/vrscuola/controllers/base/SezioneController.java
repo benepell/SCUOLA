09519819fcb30d3fb5beca738312e7447ad24527
Benedetto Pellerito <svilupporsasantangelo@gmail.com>
2023-05-28 15:33:24 +0200

    fix error username

--- src/main/java/online/vrscuola/controllers/base/SezioneController.java
+++ src/main/java/online/vrscuola/controllers/base/SezioneController.java
@@ -39,34 +39,37 @@ public class SezioneController {
 
     @PostMapping
     public String handleClasseSelection(@RequestParam("classSelected") String classSelected,@RequestParam("sectionSelected") String sectionSelected, HttpSession session) {
-        session.setAttribute("classSelected", classSelected);
-        session.setAttribute("sectionSelected", sectionSelected);
+        try {
+            session.setAttribute("classSelected", classSelected);
+            session.setAttribute("sectionSelected", sectionSelected);
 
-        keycloakUserService.initFilterSections(classSelected,sectionSelected);
-        String[] alunni = keycloakUserService.filterSectionsAllievi();
-        String[] username = keycloakUserService.filterSectionsUsername();
+            keycloakUserService.initFilterSections(classSelected,sectionSelected);
+            String[] alunni = keycloakUserService.filterSectionsAllievi();
+            String[] username = keycloakUserService.filterSectionsUsername();
 
-        session.setAttribute("usernameSelected",username);
-        // cancella i dati precedenti se cambia classe in connec sovrascrivi i dati precedenti
-        if(username != null && username.length > 0){
-            int row = studentService.deleteUserNotClass(username[0]);
-            if (row > 0) {
-                String[] alu = (String[]) session.getAttribute("alunni");
-                String[] vis = manageService.allDevices();
-                studentService.cleanVisori(session);
-                studentService.init(Arrays.asList(alu), Arrays.asList(vis));
+            session.setAttribute("usernameSelected",username);
+            // cancella i dati precedenti se cambia classe in connec sovrascrivi i dati precedenti
+            if(username != null && username.length > 0){
+                int row = studentService.deleteUserNotClass(username[0]);
+                if (row > 0) {
+                    String[] alu = (String[]) session.getAttribute("alunni");
+                    String[] vis = manageService.allDevices();
+                    studentService.cleanVisori(session);
+                    studentService.init(Arrays.asList(alu), Arrays.asList(vis));
+                }
             }
-        } else {
-            // reindirizza alla pagina di login se non username
-            return "redirect:/login";
+
+            orderService.initOrder(alunni,username,detailService);
+            String[] alunniOrdinati = orderService.getAlunni();
+            String[] usernameOrdinati = orderService.getUsername();
+            session.setAttribute("alunni",alunniOrdinati);
+            session.setAttribute("username",usernameOrdinati);
+            session.setAttribute("argoments", argomentiDirService.getArgomentiAll("aula01",classSelected,sectionSelected));
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        orderService.initOrder(alunni,username,detailService);
-        String[] alunniOrdinati = orderService.getAlunni();
-        String[] usernameOrdinati = orderService.getUsername();
-        session.setAttribute("alunni",alunniOrdinati);
-        session.setAttribute("username",usernameOrdinati);
-        session.setAttribute("argoments", argomentiDirService.getArgomentiAll("aula01",classSelected,sectionSelected));
         return "redirect:/abilita-visore";
     }
 
