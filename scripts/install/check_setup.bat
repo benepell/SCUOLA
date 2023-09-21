a6fe096cf7c4ed973fb10901e0a586078491c7e5
Benedetto Pellerito <benedettopellerito@gmail.com>
2023-09-21 02:21:34 +0200

    Details Copyright (c) 2023, Benedetto Pellerito - License apache 2.0

--- scripts/install/check_setup.bat
+++ scripts/install/check_setup.bat
@@ -1,5 +1,7 @@
 ::------------------------------------------------------------------------------
 :: Copyright (c) 2023, Benedetto Pellerito
+:: Email: benedettopellerito@gmail.com
+:: GitHub: https://github.com/benepell
 ::
 :: Licensed under the Apache License, Version 2.0 (the "License");
 :: you may not use this file except in compliance with the License.
